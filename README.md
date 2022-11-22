10ef58e6c6cd4d18b2afa0721bc4b26318ca792b
Gautam Kabiraj <51134142+gautamkabiraj96@users.noreply.github.com>
2022-11-23 01:16:17 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# vuejs-check-input-contains-numbers
-Created with CodeSandbox
+# Numbers input check in Vue.js
+
+Learn how to check if input contains any number in Vue.js | Example | Demo [Learn more](https://www.nightprogrammer.com/vue-js/check-if-input-contains-numbers-in-vue-js-example/)
---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <input
+      type="text"
+      placeholder="Type something..."
+      v-model="inputValue"
+      @keyup="inputContainsNumbers"
+    />
+    <p>
+      Contains number:
+      <span v-if="inputContainsNumber">✅ True</span>
+      <span v-else>❌ False</span>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      inputValue: null,
+      inputContainsNumber: false,
+    };
+  },
+  methods: {
+    inputContainsNumbers() {
+      const numbers = /\d/;
+      this.inputContainsNumber = numbers.test(this.inputValue);
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #000000;
+  margin-top: 60px;
+}
+input {
+  font-size: 16px;
+  padding: 4px;
+  border: 1px solid rgb(34, 34, 34);
+  outline: none;
+}
+</style>
